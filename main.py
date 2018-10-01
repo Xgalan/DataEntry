0269c39960ac6be37415545125f0fca67189dcca
Xgalan <erik_mascheri@fastmail.com>
2018-10-01 10:50:12 +0200

    Added window icon.

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import tkinter as tk
 
 from model import Model
 from view import Application
+from icons import window_image
 
 
 if __name__ == "__main__":
-    root = tk.Tk()    
+    root = tk.Tk()
+    window_icon = tk.PhotoImage(data=window_image)
     app = Application(master=root)
     model = Model(observable=app)
+    root.tk.call('wm', 'iconphoto', root._w, window_icon)
     app.master.title("Data Entry")
     app.mainloop()
