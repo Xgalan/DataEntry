2d51efcdc800294e1757d27485ce3702efa29b5d
Xgalan <erik_mascheri@fastmail.com>
2019-03-06 20:13:06 +0100

    Added confirmation to quit the app.

--- view.py
+++ view.py
@@ -8,76 +8,11 @@ from tkinter import messagebox, filedialog
 
 import icons
 import tooltip
+import dialog
 
 
 
-class Dialog(tk.Toplevel):
-    def __init__(self, parent, title = None):
-        tk.Toplevel.__init__(self, parent)
-        self.transient(parent)
-        if title:
-            self.title(title)
-        self.parent = parent
-        self.result = None
-        body = tk.Frame(self)
-        self.initial_focus = self.body(body)
-        body.pack(padx=5, pady=5)
-        self.buttonbox()
-        self.grab_set()
-        if not self.initial_focus:
-            self.initial_focus = self
-        self.protocol("WM_DELETE_WINDOW", self.cancel)
-        self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
-                                  parent.winfo_rooty()+50))
-        self.initial_focus.focus_set()
-        self.wait_window(self)
-
-    #
-    # construction hooks
-    def body(self, master):
-        # create dialog body.  return widget that should have
-        # initial focus.  this method should be overridden
-        pass
-
-    def buttonbox(self):
-        # add standard button box. override if you don't want the
-        # standard buttons
-        box = tk.Frame(self)
-        w = tk.Button(box, text="OK", width=10,
-                      command=self.ok, default=tk.ACTIVE)
-        w.pack(side=tk.LEFT, padx=5, pady=5)
-        w = tk.Button(box, text="Cancel", width=10, command=self.cancel)
-        w.pack(side=tk.LEFT, padx=5, pady=5)
-        self.bind("<Return>", self.ok)
-        self.bind("<Escape>", self.cancel)
-        box.pack()
-
-    #
-    # standard button semantics
-    def ok(self, event=None):
-        if not self.validate():
-            self.initial_focus.focus_set() # put focus back
-            return
-        self.withdraw()
-        self.update_idletasks()
-        self.apply()
-        self.cancel()
-
-    def cancel(self, event=None):
-        # put focus back to the parent window
-        self.parent.focus_set()
-        self.destroy()
-
-    #
-    # command hooks
-    def validate(self):
-        return 1 # override
-
-    def apply(self):
-        pass # override
-
-
-class SettingsDialog(Dialog):
+class SettingsDialog(dialog.Dialog):
     def body(self, master):
         tk.Label(master, text="Units:").grid(row=0)
         self.e1 = tk.Entry(master)
@@ -190,13 +125,21 @@ class Application(tk.Frame):
             self.__observers[0].units = self.d.units
             print(str(self.__observers[0].units))
 
+    def quit_dialog(self):
+        if self.data_entry.edit_modified() is 1:
+            answer = messagebox.askokcancel("Quit","Do you want to quit?")
+            if answer:
+                self.master.destroy()
+        else:
+            self.master.destroy()
+
     def create_widgets(self):
         # menu
         self.menu = tk.Frame(self, padx=2, pady=2, bd=1)
         self.menu.grid(row=0, sticky='NWE')
         # quit button
         self.quit = tk.Button(self.menu, image=self.quit_icon,
-                              command=self.master.destroy)
+                              command=self.quit_dialog)
         self.quit.image = self.quit_icon
         self.quit.grid(row=0, column=0, sticky='E')
         # minimize to tray button
